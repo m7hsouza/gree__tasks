a6d0aff636c772d98750a4f5171cf268b41d21db
Matheus Lucas M Souza <m7hsouza@gmail.com>
2022-11-07 11:51:31 -0400

    update nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,11 +61,6 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Sair') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Sair') }}
-                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
